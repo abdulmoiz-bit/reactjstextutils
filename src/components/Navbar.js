fbe4e24bc5e0d7cfaa87c269936c10a5a6ece986
Abdul Moiz Rashid <70377003+abdulmoiz-bit@users.noreply.github.com>
2022-09-05 23:20:57 +0500

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+
+export default function Navbar(props) {
+    return (
+        <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">{props.title /*props here*/}</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
+
+                    </ul>
+                    <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
+                        <input className="form-check-input" onClick={props.toggleMode} type="checkbox" id="flexSwitchCheckDefault" style={{cursor: 'pointer'}} />
+                        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Dark Mode</label>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+    aboutText: PropTypes.string.isRequired
+}
+
+Navbar.defaultProps = {
+    title: 'Set title here',
+    aboutText: 'About'
+};
